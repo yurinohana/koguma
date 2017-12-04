868ef96b66407a83c247e588034f7d32058d20c0
Yuria Aoyama <yurinohanarshi@gmail.com>
2017-12-04 12:32:16 +0000

    commit

--- app.rb
+++ app.rb
@@ -1,15 +1,12 @@
-require 'bundler/setup'
-Bundler.require
 require 'http'
 require 'json'
 require 'eventmachine'
 require 'faye/websocket'
-require 'sinatra/reloader' if development?
-require './models/koguma.rb'
 
 response = HTTP.post("https://slack.com/api/rtm.start", params: {
-  token: ENV['SLACK_API_TOKEN']
-})
+    token: ENV['SLACK_API_TOKEN']
+  })
+
 rc = JSON.parse(response.body)
 
 url = rc['url']
@@ -18,17 +15,27 @@ EM.run do
   # Web Socketインスタンスの立ち上げ
   ws = Faye::WebSocket::Client.new(url)
 
+  ws.on :open do
+    p [:open]
+  end
+
   ws.on :message do |event|
     data = JSON.parse(event.data)
     p [:message, data]
 
-    if data['text'] == 'こんばんは'
+    if data['text'] == 'こんにちは'
       ws.send({
         type: 'message',
-        text: "こんばんは <@#{data['user']}> さん",
+        text: "こんにちは <@#{data['user']}> さん",
         channel: data['channel']
         }.to_json)
     end
+  end
+
+  ws.on :close do
+    p [:close, event.code]
+    ws = nil
+    EM.stop
   end
 
 end
