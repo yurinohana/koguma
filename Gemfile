25aed378af37a0c7fe1e1f1d83cecda48fd2f132
Yuria Aoyama <yurinohanarshi@gmail.com>
2017-12-16 04:04:26 +0000

    commit

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ group :development do
   gem 'sqlite3'
 end
 group :production do
-  gem 'pg'
+  gem 'pg', '0.20.0'
 end
 gem 'json'
 gem 'http'
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# coding: utf-8
+
+Dialogue.create(:id => 2, :input => "こんにちは", :output =>"こんちゃ", :created_at => 201712161200, :updated_at => 201712161211)
