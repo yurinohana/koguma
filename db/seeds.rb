7122e7be15e57db56b1908cf706b88310e97d06f
Yuria Aoyama <yurinohanarshi@gmail.com>
2017-12-16 04:44:27 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 # coding: utf-8
 
-Dialogue.create(:id => 2, :input => "こんにちは", :output =>"こんちゃ", :created_at => 201712161200, :updated_at => 201712161211)
+Dialogue.create(:id => 1, :input => "こんにちは", :output =>"こんちゃ", :created_at => "2017-12-16 15:19:42.568828", :updated_at => "2017-12-16 15:19:42.568828")
